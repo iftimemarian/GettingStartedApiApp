ff5bf2048db40f1dcc22acad33016c8635eb6902
iftimemarian <120323882+iftimemarian@users.noreply.github.com>
2024-01-07 09:37:46 +0200

    Changes in UsersController

--- GettingStartedApi/Controllers/UsersController.cs
+++ GettingStartedApi/Controllers/UsersController.cs
@@ -4,39 +4,55 @@
 
 namespace GettingStartedApi.Controllers;
 
+//GET, POST, PUT, PATCH, DELETE
+//https://www.iamtimcorey.com  -- GET
+
+
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : ControllerBase
-{
-// GET: api/<UsersController>
-[HttpGet]
-public IEnumerable<string> Get()
-{
-    return new string[] { "value1", "value2" };
-}
+    {
+    // GET: api/Users
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return new string[] { "value1", "value2" };
+    }
 
-// GET api/<UsersController>/5
-[HttpGet("{id}")]
-public string Get(int id)
-{
-    return "value";
-}
+    // GET api/Users/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-// POST api/<UsersController>
-[HttpPost]
-public void Post([FromBody] string value)
-{
-}
+    //POST creates a new record
+    // POST api/Users
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-// PUT api/<UsersController>/5
-[HttpPut("{id}")]
-public void Put(int id, [FromBody] string value)
-{
-}
+    //PUT updates a whole record (or possibly creates)
+    // PUT api/Users/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+            //CNP, Email, PhoneNumber sensitive data
+    }
 
-// DELETE api/<UsersController>/5
-[HttpDelete("{id}")]
-public void Delete(int id)
-{
-}
+    //PATCH updates part of a record
+    //PATCH api/Users/5
+    [HttpPatch("{id}")]
+    public void Patch(int id, [FromBody] string emailAddress)
+    {
+
+    }
+
+    //DELETE deletes a record
+    // DELETE api/Users/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
 }
