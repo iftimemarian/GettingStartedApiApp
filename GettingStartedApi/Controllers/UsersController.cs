217a5ce7f8426f2d3b6baf1278202a4ad25648d6
iftimemarian <120323882+iftimemarian@users.noreply.github.com>
2024-01-07 09:42:10 +0200

    Revert "Changes in UsersController"
    This reverts commit ff5bf2048db40f1dcc22acad33016c8635eb6902.

--- GettingStartedApi/Controllers/UsersController.cs
+++ GettingStartedApi/Controllers/UsersController.cs
@@ -4,55 +4,39 @@
 
 namespace GettingStartedApi.Controllers;
 
-//GET, POST, PUT, PATCH, DELETE
-//https://www.iamtimcorey.com  -- GET
-
-
 [Route("api/[controller]")]
 [ApiController]
 public class UsersController : ControllerBase
-    {
-    // GET: api/Users
-    [HttpGet]
-    public IEnumerable<string> Get()
-    {
-        return new string[] { "value1", "value2" };
-    }
+{
+// GET: api/<UsersController>
+[HttpGet]
+public IEnumerable<string> Get()
+{
+    return new string[] { "value1", "value2" };
+}
 
-    // GET api/Users/5
-    [HttpGet("{id}")]
-    public string Get(int id)
-    {
-        return "value";
-    }
+// GET api/<UsersController>/5
+[HttpGet("{id}")]
+public string Get(int id)
+{
+    return "value";
+}
 
-    //POST creates a new record
-    // POST api/Users
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-    }
+// POST api/<UsersController>
+[HttpPost]
+public void Post([FromBody] string value)
+{
+}
 
-    //PUT updates a whole record (or possibly creates)
-    // PUT api/Users/5
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-            //CNP, Email, PhoneNumber sensitive data
-    }
+// PUT api/<UsersController>/5
+[HttpPut("{id}")]
+public void Put(int id, [FromBody] string value)
+{
+}
 
-    //PATCH updates part of a record
-    //PATCH api/Users/5
-    [HttpPatch("{id}")]
-    public void Patch(int id, [FromBody] string emailAddress)
-    {
-
-    }
-
-    //DELETE deletes a record
-    // DELETE api/Users/5
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-    }
+// DELETE api/<UsersController>/5
+[HttpDelete("{id}")]
+public void Delete(int id)
+{
+}
 }
